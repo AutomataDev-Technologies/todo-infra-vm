f24d36f0d3da4965bae329c28578b531f77da516
Satya Prakash <spsingh302212@gmail.com>
2025-12-18 13:08:38 +0530

    Add files via upload

---
+++ ENV/Dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "statefile-rg"
+    storage_account_name = "statefilestorage12"
+    container_name       = "statecontainer"
+    key                  = "dev.terraform.tfstate"
+  }
+}
---
+++ MODULE/azurerm_key_vault_secret/azurerm_key_vault_secret.tf
@@ -0,0 +1,46 @@
+# resource "azurerm_key_vault_secret" "secret" {
+#   for_each   = var.secret
+#   name       = each.value.secret-name
+#   value      = each.value.secret-value
+#   #   key_vault_id = data.azurerm_key_vault.secret[each.key].id
+#   key_vault_id = azurerm_key_vault.kv[each.value.keyvault_name].id
+
+# }
+
+// resource "azurerm_key_vault_secret" "secret" {
+//   for_each = var.secret
+
+//   name  = each.value.secret_name
+//   value = each.value.secret_value
+//   #   key_vault_id = data.azurerm_key_vault.secret[each.value.keyvault_name]
+//   key_vault_id = data.azurerm_key_vault.secret[each.key].id
+
+//   depends_on = [
+//     null_resource.purge_deleted_secrets
+//   ]
+// }
+
+// resource "azurerm_key_vault_secret" "secret" {
+//   for_each = var.secret
+
+//   name         = each.value.secret_name
+//   value        = each.value.secret_value
+//   key_vault_id = data.azurerm_key_vault.secret[each.key].id
+
+//   lifecycle {
+//     ignore_changes = [value] # optional, safer
+//   }
+// }
+
+resource "azurerm_key_vault_secret" "secret" {
+  for_each = var.secret
+
+  name         = each.value.secret_name
+  value        = each.value.secret_value
+  key_vault_id = data.azurerm_key_vault.secret[each.key].id
+
+  lifecycle {
+    ignore_changes = [value]        # optional, safer for updates
+    prevent_destroy = true          # accidental deletion se bachata hai
+  }
+}
